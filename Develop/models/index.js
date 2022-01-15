cfc874379c2934647b9dd3fb6f54d03de96e19c1
anabwarsame <anab_1994@hotmail.co.uk>
2022-01-15 18:09:43 +0000

    index model models completed

---
+++ Develop/models/index.js
@@ -0,0 +1,40 @@
+// import models
+const Product = require("./Product");
+const Category = require("./Category");
+const Tag = require("./Tag");
+const ProductTag = require("./ProductTag");
+
+Product.belongsTo(Category, {
+  foreignKey: "category_id",
+  onDelete: "CASCADE",
+});
+
+Category.hasMany(Product, {
+  foreignKey: "category_id",
+});
+
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  foreignKey: "product_id",
+  onDelete: "CASCADE",
+});
+
+Tag.belongsToMany(product, {
+  through: ProductTag,
+  foreignKey: "tag_id",
+  onDelete: "CASCADE",
+});
+// Products belongsTo Category
+
+// Categories have many Products
+
+// Products belongToMany Tags (through ProductTag)
+
+// Tags belongToMany Products (through ProductTag)
+
+module.exports = {
+  Product,
+  Category,
+  Tag,
+  ProductTag,
+};
