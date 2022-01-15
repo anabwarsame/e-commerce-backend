6607848bfa9363c16b64e0554fc4d5f06e9e20aa
anabwarsame <anab_1994@hotmail.co.uk>
2022-01-15 21:01:19 +0000

    completed get routes for tag routes

--- src/routes/api/tag-routes.js
+++ src/routes/api/tag-routes.js
@@ -1,30 +1,44 @@
-const { Router } = require('express');
+const { Router } = require("express");
 
-const { Tag, Product, ProductTag } = require('../../models');
+const { Tag, Product, ProductTag } = require("../../models");
 
 // The `/api/tags` endpoint
 
 const router = Router();
 
-router.get('/', (req, res) => {
-  // find all tags
-  // be sure to include its associated Product data
+router.get("/", async (req, res) => {
+  try {
+    const getTags = await Category.findAll({
+      include: {
+        model: Tag,
+        attributes: ["id", "product_name", "price", "stock", "category_id"],
+      },
+    });
+
+    if (!getTags) {
+      return res.status(404).json({ message: "can not find tag" });
+    }
+    return res.json(getTags);
+  } catch (error) {
+    console.error(`[ERROR]: failed to render tags | ${error.message}`);
+    return res.status(500).json({ error: "failed to render tags" });
+  }
 });
 
-router.get('/:id', (req, res) => {
+router.get("/:id", (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
 });
 
-router.post('/', (req, res) => {
+router.post("/", (req, res) => {
   // create a new tag
 });
 
-router.put('/:id', (req, res) => {
+router.put("/:id", (req, res) => {
   // update a tag's name by its `id` value
 });
 
-router.delete('/:id', (req, res) => {
+router.delete("/:id", (req, res) => {
   // delete on tag by its `id` value
 });
 
